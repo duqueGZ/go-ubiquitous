3b34a8c9d9beb484cc0a49ede4bebd49bf35e5d8
David Duque Fuertes <david.duque.f@gmail.com>
2016-05-13 19:36:00 +0200

    refactor: Fixes some problems with package structure in wearable module (it didn't match with the structure in app module)

--- wearable/src/main/java/com/example/android/sunshine/app/SunshineWatchFace.java
+++ wearable/src/main/java/com/example/android/sunshine/app/SunshineWatchFace.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.wearable;
+package com.example.android.sunshine.app;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -31,27 +31,20 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.wearable.watchface.CanvasWatchFaceService;
 import android.support.wearable.watchface.WatchFaceStyle;
-import android.text.format.DateFormat;
-import android.text.format.Time;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.WindowInsets;
 
+import com.example.android.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.DataEvent;
 import com.google.android.gms.wearable.DataEventBuffer;
-import com.google.android.gms.wearable.DataItem;
-import com.google.android.gms.wearable.DataMap;
-import com.google.android.gms.wearable.DataMapItem;
 import com.google.android.gms.wearable.Wearable;
 
 import java.lang.ref.WeakReference;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
